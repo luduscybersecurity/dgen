0192438ac7a10a6512bd441c0482e061a89f4163
George Stewart <george@luduscybersecurity.com>
2019-03-06 22:42:51 +1100

    new script to start the container in a shell

---
+++ dgen-shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TEMPLATE_MOUNT=""
+if [ $DGEN_TEMPLATES_ROOT ]
+then
+    TEMPLATE_MOUNT="--mount type=bind,source=$DGEN_TEMPLATES_ROOT,target=$DGEN_TEMPLATES_ROOT"
+fi
+
+docker run --mount type=bind,source="$(pwd)",target=/project $TEMPLATE_MOUNT\
+    --mount type=bind,source=$HOME/.ssh,target=$HOME/.ssh,readonly\
+    --mount type=bind,source=/etc/passwd,target=/etc/passwd,readonly\
+    --user $(id -u):$(id -g) --rm -it dgen:latest /bin/bash
