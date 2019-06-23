7a6282f623e1ad3c9a6c74f0d2104838100af9bc
George Stewart <george@luduscybersecurity.com>
2019-06-23 22:59:38 +1000

    metavars in URLs and Code

--- metavars.py
+++ metavars.py
@@ -1,67 +1,86 @@
 #!/usr/bin/env python
 
-"""
-Pandoc filter to allow interpolation of metadata fields
-into a document.  %{fields} will be replaced by the field's
-value.
-"""
+import dgen_utils
+from panflute import Link, Str, ListContainer, Code
+import panflute
 
-import re, codecs, sys, urllib2
-from collections import OrderedDict
+import re
+import codecs
+import sys
+import urllib2
 
 # disable annoying warning from click caused by panflute. An upgrade to python 3 might fix
 import click
 click.disable_unicode_literals_warning = True
-import panflute
-from panflute import Link, Str
 
-
-import dgen_utils
 
 #import ptvsd
 # 5678 is the default attach port in the VS Code debug configurations
 #print("Waiting for debugger attach")
 #ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
-#ptvsd.wait_for_attach()
-#breakpoint()
+# ptvsd.wait_for_attach()
+# breakpoint()
 
 PATTERN = None
 PATTERN_BAD = None
+
 
 def init_metavars(doc):
     global PATTERN, PATTERN_BAD
     PATTERN = re.compile(r'(.*)%{(.*?)}(.*)')
     PATTERN_BAD = re.compile(r'(\${.*?})')
 
-def check_str(elem, document):
-    text = urllib2.unquote(elem.text)
 
+def filter_text(text, document):
+    text = urllib2.unquote(text)
     for match in PATTERN.finditer(text):
         #dgen_utils.log_warn(':'.join([str(elem.parent),elem.text, text, match.group(1), match.group(2), match.group(3)]))
         field = match.group(2)
         result = document.get_metadata(field, None)
         if isinstance(result, unicode):
-            return Str(match.group(1) + result + match.group(3))
+            return match.group(1) + result + match.group(3)
         dgen_utils.log_warn("metavar not found in document:", field)
     match_bad = PATTERN_BAD.match(text)
     if match_bad:
-        dgen_utils.log_warn("found wrong syntax for metavar:", match_bad.group(1))
-    return None
+        dgen_utils.log_warn(
+            "found wrong syntax for metavar:", match_bad.group(1))
+    return text
+
+
+def filter_code(elem, document):
+    return elem
+
+
+def filter_str(elem, document):
+    return Str(filter_text(elem.text, document))
+
+
+def filter_link(elem, document):
+    for item in elem.content:
+        item = filter(item, document)
+    elem.url = filter_text(elem.url, document)
+    elem.title = filter_text(elem.title, document)
+    return elem
+
 
 def filter(elem, document):
     if type(elem) == Str:
-        return check_str(elem, document)
+        return filter_str(elem, document)
     if type(elem) == Link:
-        #dgen_utils.log_warn("Link elemement found:", elem)
-        pass
+        return filter_link(elem, document)
+    if type(elem) == Code:
+        return filter_code(elem, document)
     return None
+
 
 def finalize(doc):
     pass
 
+
 def main(doc=None):
-    input_stream=codecs.getreader('utf8')(sys.stdin)
+    input_stream = codecs.getreader('utf8')(sys.stdin)
     return panflute.run_filter(filter, prepare=init_metavars, finalize=finalize, doc=doc, input_stream=input_stream)
+
 
 if __name__ == '__main__':
     main()
