bd5e02cb4037f9c8f1d960598d58d19c01c226c7
gee <george@luduscybersecurity.com>
2020-09-28 23:04:39 +1000

    add support for https repos to docker container

--- dgen.sh
+++ dgen.sh
@@ -9,7 +9,9 @@ fi
 
 SCRIPT="groupadd -g $(id -g) ${USER} && useradd -u $(id -u) -g $(id -g) -M -d ${HOME} -l ${USER} && su -c '/app/dgen.py ${@:1}' ${USER}"
 
-docker run --mount type=bind,source="$(pwd)",target=/project $TEMPLATE_MOUNT\
-    --mount type=bind,source=$HOME/.dgen,target=$HOME/.dgen\
-    --mount type=bind,source=$HOME/.ssh,target=$HOME/.ssh,readonly\
+docker run --mount type=bind,source="$(pwd)",target=/project $TEMPLATE_MOUNT \
+    --mount type=bind,source=$HOME/.gitconfig,target=$HOME/.gitconfig,readonly \
+    --mount type=bind,source=$HOME/.git-credentials,target=$HOME/.git-credentials,readonly \
+    --mount type=bind,source=$HOME/.dgen,target=$HOME/.dgen \
+    --mount type=bind,source=$HOME/.ssh,target=$HOME/.ssh,readonly \
     --rm -it dgen:latest /bin/bash -c "${SCRIPT}"
